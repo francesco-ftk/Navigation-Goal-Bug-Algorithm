f2253f60304c8b7635bc66f14cb164bf2fe3b644
Francesco Fantechi <51874619+francesco-ftk@users.noreply.github.com>
2023-03-09 22:00:37 +0100

    Delete AA&IR laboratory bug algorithm directory

--- AA&IR laboratory bug algorithm/gym_navigation/__init__.py
+++
@@ -1,7 +0,0 @@
-""" This __init__ file is used to register the environment """
-from gym.envs.registration import register
-
-register(
-    id='NavigationGoal-v0',
-    entry_point='gym_navigation.envs.navigation_goal:NavigationGoal')
-
